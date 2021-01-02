86bedd67843c5885862bed9b50c2272180f8e6b2
Marcos Cid de la Paz <markgorithm@icloud.com>
2021-01-02 10:16:44 -0600

    Configuración del componente principal para que muestre el listado de usuarios

--- src/actions/index.js
+++ src/actions/index.js
@@ -8,14 +8,11 @@ export function showUsers() {
     { id: 1, name: "Marcos" },
     { id: 2, name: "Fanny" },
     { id: 3, name: "Fernanda" },
-    {
-      id: 4,
-      name: "Marcos",
-    },
+    { id: 4, name: "Marcos" },
   ];
   return {
     //return de un objeto coyo tipo es SHOW_USERS y cuyo payload es un listado de usuarios
     type: SHOW_USERS,
-    payload: [],
+    payload: users,
   };
 }
--- src/components/app.js
+++ src/components/app.js
@@ -1,9 +1,36 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { showUsers } from "../actions";
 
-export default class App extends Component {
+// Configurar el componente principal para que muestre el listado de usuarios
+class App extends Component {
+  //La función debe llamarse cuando el componente se cargue
+  componentWillMount() {
+    this.props.showUsers();
+  }
+
+  renderUsersList() {
+    return this.props.users.map((user) => {
+      return <li key={user.id}>{user.name}</li>;
+    });
+  }
+
   render() {
     return (
-      <div>React simple starter</div>
+      <div>
+        <h2>Users List</h2>
+        <ul>{this.renderUsersList()}</ul>
+      </div>
     );
   }
 }
+
+// Función de mapeo del state a propiedades (del store general de redux)
+function mapStateToProps(state) {
+  return {
+    users: state.user.list,
+  };
+}
+
+// Se debe pasar una función con un mapeo y la aplicación. Pasar en el mapeo la acción como si fuese una función más que está en las propiedades del componente
+export default connect(mapStateToProps, { showUsers })(App);
